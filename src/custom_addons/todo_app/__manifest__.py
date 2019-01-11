9ee2cc96e75c94e75a8df0b70fe4c32d0876ddaf
Lansana Barry Sow <lansanabarry97@gmail.com>
2019-01-11 11:49:28 +0100

    [ADD] Modulo 'To-do Task'

---
+++ src/custom_addons/todo_app/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'To-Do Application',
+    'description': 'Manage personal to-do tasks.',
+    'author': 'Lansana Barry Sow',
+    'depends': ['base'],
+    'application': True,
+
+    'data': [
+        'views/todo_menu.xml',
+        'views/todo_view.xml',
+    ],
+}
